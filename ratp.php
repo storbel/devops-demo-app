0cd37f4965f4d086693e4be2b72d12c83c0af1af
storbel <contact@storbel.com>
2020-01-15 14:54:55 +0100

    added api

--- ratp.php
+++ ratp.php
@@ -6,27 +6,7 @@ $config = parse_ini_file('config.ini');
 $dbSuccess = false;
 $dbVersion = false;
 
-// Connect to SQL Server
-$conn = @new mysqli($config['hostname'], $config['username'], $config['password'], $config['dbname']);
-
-if ($conn->connect_errno) {
-}
-else {
-    $dbSuccess = true;
-}
-$sql = "SELECT version FROM dbversion ORDER BY id DESC LIMIT 1";
-$result = $conn->query($sql);
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        $dbVersion = $row['version'];
-    }
-}
-
-
-// Close Connection
-$conn->close(); ?>
-
+?>
 <!DOCTYPE html>
 <html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 
@@ -37,7 +17,7 @@ $conn->close(); ?>
     <meta name="author" content="">
     <link rel="icon" href="book.ico">
 
-    <title>DevOps Demo Application</title>
+    <title>RATP API Application</title>
 
     <!-- Bootstrap core CSS -->
     <link href="bootstrap.min.css" rel="stylesheet">
@@ -53,6 +33,14 @@ $conn->close(); ?>
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+    
+ <style>
+body {background-color: powderblue;}
+    div {
+  background-image: url("https://mdn.mozillademos.org/files/6457/mdn_logo_only_color.png");
+}
+    </style>
+    
 </head>
 
 <body>
@@ -64,48 +52,46 @@ $conn->close(); ?>
         <div class="cover-container">
 
             <div class="inner cover">
-                <h1 class="cover-heading">Welcome to the DevOps Demo Application Openshift Based</h1>
+                <h1 class="cover-heading">RATP API QUERY APPLICATION</h1>
                 <p class="lead"><i>This app is used for demonstrating and testing various DevOps, CI, and CD concepts.</i></p>
-                <h3>Connection to MySQL DB:
-                    <?php
-                        if ($dbSuccess) {echo '<span class="label label-success">Success';}
-                        else {echo '<span class="label label-danger">Failure';}
-                    ?>
-                    </span>
-                </h3>
-                <h3>Environment: <span class="label label-info"><?php echo $config['environment'] ?></span></h3>
-                <h3>Application Version: <span class="label label-info">1.7</span></h3>
-                <h3>Database Scripts Version: <?php
-                        if ($dbVersion) {echo '<span class="label label-info">'. $dbVersion;}
-                        else {echo '<span class="label label-danger">Failed to load version data';}
-                        ?></span></h3>
-            </div>
-            <div>
-                <?php
-                            $dir = "/files/";
+                
+                
+                <form action="" method="POST">
+<label>Enter Order ID:</label><br />
+<input type="text" name="type_ligne" placeholder="Entrer type de ligne" required/>
+                    <br /><br /><span><ul>
+                                    <li>metros</li>
+                    <li>tramways</li>
+                    <li>buses</li>
+                    <li>rers</li>
+                    <li>noctiliens</li>
+                                    <span>
+<button type="submit" name="submit">Submit</button>
+</form>
+                
+                
+    <?php
+if (isset($_POST['type_ligne']) && $_POST['type_ligne']!="") {
+ type_ligne = $_POST['type_ligne'];
+ $url = "https://api-ratp.pierre-grimaud.fr/v4/lines/".$type_ligne;
+ 
+ $client = curl_init($url);
+ curl_setopt($client,CURLOPT_RETURNTRANSFER,true);
+ $response = curl_exec($client);
+ 
+ $result = json_decode($response);
+ 
+ echo "<table>";
+ echo "<tr><td>Code:</td><td>$result->code</td></tr>";
+ echo "<tr><td>Name:</td><td>$result->name</td></tr>";
+ echo "<tr><td>Directions:</td><td>$result->directions</td></tr>";
+ echo "<tr><td>Id:</td><td>$result->id</td></tr>";
+                    
+ echo "</table>";
+}
+    ?>            
 
-
-                            // Sort in descending order
-                            $folder= scandir($dir,1);
-                            echo '
-                            <div class="alert alert-info" role="alert">
-                              File listing of /files/
-                            </div>';
-                                       echo '<table class="table">
-                                                <thead class="thead-light">
-                                                     <tr><th>File name</th></tr>
-                                                 </thead>';     
-                                            foreach ($folder as &$file) {
-                                                echo "<tr><td>".$value."</td></tr>";
-                                            }
-                                            echo "</table>";
-                ?>
-            </div>
-            <div class="mastfoot">
-                <div class="inner">
-                    <p>DevOps Demo application provided by the <a href="https://www.devopslibrary.com/">DevOps Library</a>.</p>
-                </div>
-            </div>
+           
         </div>
     </div>
 </div>
